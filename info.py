38fa4bd2ba34e62c8658e8213066518c125468c0
Avinna09 <cmahalik216@gmail.com>
2025-07-24 22:12:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -100,9 +100,9 @@ LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002281894894')) #Verificat
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002336361980')) #If Anyone Set Your Bot In Any Group And Set Shortner In That Group Then In This Channel The All Details Come
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
 
-TUTORIAL = environ.get("TUTORIAL", "")   # Tutorial link for verification
-TUTORIAL_2 = environ.get("TUTORIAL_2", "")   # Second tutorial link for verification
-TUTORIAL_3 = environ.get("TUTORIAL_3", "")   # Third tutorial link for verification
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/movie_star65/8")   # Tutorial link for verification
+TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/movie_star65/8")   # Second tutorial link for verification
+TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/movie_star65/8")   # Third tutorial link for verification
 
 # Verification (Must Fill All Veriables. Else You Got Error
 SHORTENER_API = environ.get("SHORTENER_API", "26410ed8fe39767b403da1e477c32ffaf3e458cc") # Shortener API key
